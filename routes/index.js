f5291f3ce184375cdf5eeb7627f0e2cf596ffcdd
Jonas Flodén <jonas@koalasoft.se>
2018-02-26 19:10:12 +0100

    First version of login API

--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,14 @@ const router = express.Router();
 // Routes
 const customers = require('./customers');
 const genres = require('./genres');
+const logins = require('./logins');
 const movies = require('./movies');
 const rentals = require('./rentals');
 const users = require('./users');
 
 router.use('/api/customers', customers);
 router.use('/api/genres', genres);
+router.use('/api/logins', logins);
 router.use('/api/movies', movies);
 router.use('/api/rentals', rentals);
 router.use('/api/users', users);
---
+++ routes/logins.js
@@ -0,0 +1,33 @@
+
+const express = require('express');
+const router = express.Router();
+const Joi = require('joi');
+const bcrypt = require('bcrypt');
+const {User} = require('../models');
+
+function validate(genre) {
+    const schema = {
+        email: Joi.string().min(3).required().email(),
+        password: Joi.string().min(5).required()
+    };
+
+    return Joi.validate(genre, schema);
+}
+
+router.post('/', async (req, res) => {
+    const { error } = validate(req.body);
+    if (error)
+        return res.status(400).send(error.details[0].message);
+
+    let user = await User.findOne({ email: req.body.email });
+    if (!user)
+        return res.status(400).send('Invalid email or password');
+
+    let validPassword = await bcrypt.compare(req.body.password, user.password);
+    if (!validPassword)
+        return res.status(400).send('Invalid email or password');
+
+    res.send(true);
+});
+
+module.exports = router;
