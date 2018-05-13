892caba8e84515cee3fdc603b8588388788a6a87
Jonas Flodén <jonas@koalasoft.se>
2018-05-13 14:52:24 +0200

    Add CORS

--- startup/express.js
+++ startup/express.js
@@ -4,11 +4,17 @@ const helmet = require('helmet');
 const morgan = require('morgan');
 const error = require('../middleware/error')
 const winston = require('winston');
+const cors = require('cors');
 
 const routes = require('../routes');
 
+const corsOptions = {
+  origin: 'https://localhost:4200',
+}
+
 const app = express();
 app.use(express.json());
+app.use(cors(corsOptions));
 app.use(helmet());
 app.use(morgan('tiny'));
 app.use('/', routes);
